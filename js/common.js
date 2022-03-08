3f7d0438c84d15409ea46afdbcf419133a1779ff
Naoko Yamaguchi <kamaki.naoko@hotmail.co.jp>
2022-03-08 11:50:31 +0900

    0308commit

--- js/common.js
+++ js/common.js
@@ -1,27 +1,23 @@
 $(function () {
+  
     AOS.init();
-
-    $(".main-visual").vegas({
-        timer: false,
-        ovarlay: false,
-        transition: 'blur',
-        transitionDuration: 2000,
-        delay: 10000,
-        animation: 'kenburns',
-        animationDuration: 10000,
-        slides: [{
-                src: "./img/photo01.jpg"
-            },
-            {
-                src: "./img/photo011.jpg"
-            },
-            {
-                src: "./img/photo012.jpg"
-            },
-        ]
+    
+    $('.main-visual').vegas({
+      overlay: false,
+      transition: 'blur', 
+      transitionDuration: 2000,
+      delay: 10000,
+      animation: 'kenburns',
+      animationDuration: 10000,
+      slides: [
+        { src: './img/photo01.jpg' },
+        { src: './img/photo011.jpg' },
+        { src: './img/photo012.jpg' },
+      ]
     });
-
+  
     $(document).ready(function () {
         $('.drawer').drawer();
     });
-})
+  
+  })
